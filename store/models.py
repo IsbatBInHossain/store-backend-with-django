b71bf9fafca343bbb8f41ada7ddf920f5da6754a
Isbat-Bin-Hossain <isbat31416@gmail.com>
2024-03-24 22:09:47 +0600

    Make migrations

--- store/models.py
+++ store/models.py
@@ -11,7 +11,8 @@ class Promotion(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField()
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -37,6 +38,7 @@ class Customer(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=10, null=True)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
 class Order(models.Model):
