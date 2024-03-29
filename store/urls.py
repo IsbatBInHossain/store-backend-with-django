04b7762c5f2f1e368381a6bcc6fa49f622385f3f
Isbat-Bin-Hossain <isbat31416@gmail.com>
2024-03-30 01:39:26 +0600

    Complete the Cart and CartItem api

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,16 @@
-from .views import ProductViewSet, CollectionViewSet, ReviewViewSet
+from .views import ProductViewSet, CollectionViewSet, ReviewViewSet, CartViewSet, CartItemViewSet
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='products')
 router.register('collections', CollectionViewSet)
+router.register('carts', CartViewSet)
 
 product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 product_router.register('reviews', ReviewViewSet, basename='product-reviews')
 
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('items', CartItemViewSet, basename='cart-items')
 
-urlpatterns = router.urls + product_router.urls
+
+urlpatterns = router.urls + product_router.urls + cart_router.urls
